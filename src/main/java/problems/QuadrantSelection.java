85fec8371dcd49ff34e89b0ee66da56d7cbd1e4d
softish <lazar.h.klincov@live.se>
2017-09-29 20:31:49 +0200

    Solved Quadrant Selection.

---
+++ src/main/java/problems/QuadrantSelection.java
@@ -0,0 +1,31 @@
+package problems;
+
+import java.util.Scanner;
+
+/**
+ * Created by softish on 2017-09-29.
+ */
+public class QuadrantSelection {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int quadrant = deduceQuadrant(scanner.nextInt(), scanner.nextInt());
+
+        System.out.println(quadrant);
+    }
+
+    private static int deduceQuadrant(int x, int y) {
+        if(isPositive(x) && isPositive(y))
+            return 1;
+        else if(!isPositive(x) && isPositive(y))
+            return  2;
+        else if(!isPositive(x) && !isPositive(y))
+            return 3;
+        else
+            return 4;
+    }
+
+    private static boolean isPositive(int number) {
+        return number > 0;
+    }
+}
