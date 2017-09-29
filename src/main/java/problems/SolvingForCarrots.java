b7615a17420c7d81bc70fd8c78f2915de0b47b31
softish <lazar.h.klincov@live.se>
2017-09-29 20:31:08 +0200

    Solved Solving for Carrots.

---
+++ src/main/java/problems/SolvingForCarrots.java
@@ -0,0 +1,17 @@
+package problems;
+
+import java.util.Scanner;
+
+/**
+ * Created by softish on 2017-09-29.
+ */
+public class SolvingForCarrots {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        String[] numbers = input.split(" ");
+
+        System.out.println(numbers[1]);
+    }
+}
