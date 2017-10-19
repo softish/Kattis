15868a1006c9e44eb2ed405899124585aa09a231
softish <lazar.h.klincov@live.se>
2017-10-19 20:55:41 +0200

    Solved Faktor.

---
+++ src/main/java/problems/Faktor.java
@@ -0,0 +1,29 @@
+package problems;
+
+import java.util.Scanner;
+
+/**
+ * Created by softish on 2017-10-02.
+ */
+public class Faktor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()) {
+            int numberOfArticles = scanner.nextInt();
+            int impactFactor = scanner.nextInt();
+            double roundedImpactFactor = impactFactor;
+
+            if(impactFactor > 1) {
+                roundedImpactFactor = impactFactor - 0.99;
+            }
+
+            int result = calculateResult(numberOfArticles, roundedImpactFactor);
+            System.out.println(result);
+        }
+    }
+
+    private static int calculateResult(int numberOfArticles, double roundedImpactFactor) {
+        return (int) Math.ceil(numberOfArticles * roundedImpactFactor);
+    }
+}
