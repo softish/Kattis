74459f05b81205a54768414a228badba618d0bde
softish <lazar.h.klincov@live.se>
2018-01-30 21:50:53 +0100

    Refactored Trik to use the state pattern.

--- src/main/java/problems/Trik.java
+++ src/main/java/problems/Trik.java
@@ -8,28 +8,117 @@ import java.util.Scanner;
 public class Trik {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int currentIndex = 1;
+        CupHandler cupHandler = new CupHandler();
 
         while (scanner.hasNext()) {
             String line = scanner.nextLine();
 
             for (Character character : line.toCharArray()) {
-                if (character.equals('A') && currentIndex == 1) {
-                    currentIndex = 2;
-                } else if (character.equals('A') && currentIndex == 2) {
-                    currentIndex = 1;
-                } else if (character.equals('B') && currentIndex == 2) {
-                    currentIndex = 3;
-                } else if (character.equals('B') && currentIndex == 3) {
-                    currentIndex = 2;
-                } else if (character.equals('C') && currentIndex == 1) {
-                    currentIndex = 3;
-                } else if (character.equals('C') && currentIndex == 3) {
-                    currentIndex = 1;
+                if (character.equals('A')) {
+                    cupHandler.invokeActionA();
+                } else if (character.equals('B')) {
+                    cupHandler.invokeActionB();
+                } else if (character.equals('C')) {
+                    cupHandler.invokeActionC();
                 }
             }
 
-            System.out.println(currentIndex);
+            System.out.println(cupHandler.getCurrentIndex());
         }
+    }
+}
+
+abstract class State {
+    public abstract int getCurrentIndex();
+
+    public State actionA() {
+        return this;
+    }
+
+    public State actionB() {
+        return this;
+    }
+
+    public State actionC() {
+        return this;
+    }
+}
+
+class CupOne extends State {
+
+    @Override
+    public int getCurrentIndex() {
+        return 1;
+    }
+
+    @Override
+    public State actionA() {
+        return new CupTwo();
+    }
+
+    @Override
+    public State actionC() {
+        return new CupThree();
+    }
+}
+
+class CupTwo extends State {
+
+    @Override
+    public int getCurrentIndex() {
+        return 2;
+    }
+
+    @Override
+    public State actionA() {
+        return new CupOne();
+    }
+
+    @Override
+    public State actionB() {
+        return new CupThree();
+    }
+}
+
+
+class CupThree extends State {
+
+    @Override
+    public int getCurrentIndex() {
+        return 3;
+    }
+
+    @Override
+    public State actionB() {
+        return new CupTwo();
+    }
+
+    @Override
+    public State actionC() {
+        return new CupOne();
+    }
+}
+
+class CupHandler {
+    private State currentState;
+
+    CupHandler() {
+        currentState = new CupOne();
+    }
+
+    public void invokeActionA() {
+        currentState = currentState.actionA();
+    }
+
+    public void invokeActionB() {
+        currentState = currentState.actionB();
+    }
+
+    public void invokeActionC() {
+        currentState = currentState.actionC();
+    }
+
+    public int getCurrentIndex() {
+        return currentState.getCurrentIndex();
     }
 }
