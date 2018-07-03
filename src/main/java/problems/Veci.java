c3d6d98681258b06dc183c55f52a50c16d775d29
softish <lazar.h.klincov@live.se>
2018-07-03 18:59:48 +0200

    Refactored Veci solution to not use external dependencies.

--- src/main/java/problems/Veci.java
+++ src/main/java/problems/Veci.java
@@ -1,40 +1,73 @@
 package problems;
 
-import com.google.common.collect.HashMultiset;
-import com.google.common.collect.Multiset;
-
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by softish on 2018-05-10.
  */
 public class Veci {
 
+    private static Set<Integer> permutationsOfNumber;
+
+    static {
+        permutationsOfNumber = new HashSet<>();
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNext()) {
-            System.out.println(sameDigits(scanner.nextInt()));
+            System.out.println(findClosestNumberWithSameDigits(scanner.nextInt()));
         }
     }
 
-    static int sameDigits(int number) {
-        for (int i = number + 1; i < 999_999; i++) {
-            if (digitDecomposition(i).equals(digitDecomposition(number))) {
-                return i;
+    static int findClosestNumberWithSameDigits(int number) {
+        permutateNumber(number);
+        Map<Integer, Integer> distances = measureDistance(number);
+        Integer minimumDistanceKey = findShortestDistance(distances);
+        Integer closestNumber = distances.get(minimumDistanceKey);
+        return closestNumber == null ? 0 : closestNumber;
+    }
+
+    /**
+     * Finds all permutationsOfNumber of the provided number.
+     * Source: https://introcs.cs.princeton.edu/java/23recursion/Permutations.java.html
+     *
+     * @param number number to permutate
+     */
+    private static void permutateNumber(int number) {
+        permutateNumber("", String.valueOf(number));
+    }
+
+    private static void permutateNumber(String prefix, String number) {
+        int length = number.length();
+        if (length == 0) {
+            permutationsOfNumber.add(Integer.parseInt(prefix));
+        } else {
+            for (int i = 0; i < number.length(); i++) {
+                permutateNumber(prefix + number.charAt(i), number.substring(0, i) + number.substring(i + 1, length));
             }
         }
-
-        return 0;
     }
 
-    static Multiset<Integer> digitDecomposition(int number) {
-        char[] digits = Integer.toString(number).toCharArray();
-        Multiset<Integer> set = HashMultiset.create();
+    private static Map<Integer, Integer> measureDistance(int number) {
+        Map<Integer, Integer> permutationDistanceMapping = new HashMap<>();
 
-        for (char digit : digits) {
-            set.add(Integer.parseInt(String.valueOf(digit)));
+        for (Integer permutation : permutationsOfNumber) {
+            int distance = permutation - number;
+            permutationDistanceMapping.put(distance, permutation);
         }
 
-        return set;
+        permutationsOfNumber.clear();
+        return permutationDistanceMapping;
+    }
+
+    private static Integer findShortestDistance(Map<Integer, Integer> distances) {
+        Integer minimumDistanceKey = Integer.MAX_VALUE;
+        for (Integer integer : distances.keySet()) {
+            if (integer > 0 && integer < minimumDistanceKey) {
+                minimumDistanceKey = integer;
+            }
+        }
+        return minimumDistanceKey;
     }
 }
--- src/test/java/problems/VeciTest.java
+++ src/test/java/problems/VeciTest.java
@@ -1,11 +1,6 @@
 package problems;
 
-import com.google.common.collect.HashMultiset;
-import com.google.common.collect.Multiset;
 import org.junit.Test;
-
-import java.util.Arrays;
-import java.util.Collections;
 
 import static org.junit.Assert.assertEquals;
 
@@ -16,37 +11,19 @@ public class VeciTest {
 
     @Test
     public void sameDigitsSampleInput1() {
-        int result = Veci.sameDigits(156);
+        int result = Veci.findClosestNumberWithSameDigits(156);
         assertEquals(165, result);
     }
 
     @Test
     public void sameDigitsSampleInput2() {
-        int result = Veci.sameDigits(330);
+        int result = Veci.findClosestNumberWithSameDigits(330);
         assertEquals(0, result);
     }
 
     @Test
     public void sameDigitsSampleInput3() {
-        int result = Veci.sameDigits(27711);
+        int result = Veci.findClosestNumberWithSameDigits(27711);
         assertEquals(71127, result);
-    }
-
-    @Test
-    public void digitDecompositionTest() {
-        Multiset result = Veci.digitDecomposition(1);
-        assertEquals(HashMultiset.create(Collections.singletonList(1)), result);
-    }
-
-    @Test
-    public void digitDecompositionTest2() {
-        Multiset result = Veci.digitDecomposition(12);
-        assertEquals(HashMultiset.create(Arrays.asList(1, 2)), result);
-    }
-
-    @Test
-    public void digitDecompositionTest3() {
-        Multiset result = Veci.digitDecomposition(101);
-        assertEquals(HashMultiset.create(Arrays.asList(1, 0, 1)), result);
     }
 }
