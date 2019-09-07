a685057e4bcb84f97088acdfa4bf17617bc212c1
softish <lazar.h.klincov@live.se>
2019-09-07 19:34:48 +0200

    Reduced error message complexity for Spavanac

--- src/main/java/problems/Spavanac.java
+++ src/main/java/problems/Spavanac.java
@@ -2,74 +2,28 @@ package problems;
 
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Scanner;
 
 public class Spavanac {
 
     private static final int REWIND_BY_MINUTES_FACTOR = 45;
+    private static final String TIME_PATTERN = "H m";
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNext()) {
-            String line = scanner.nextLine();
-            TimestampInputParser timestampInputParser = new TimestampInputParser();
             try {
-                timestampInputParser.parse(line);
-                String rewindedTime = rewindTimeByFactor(timestampInputParser.getHours(), timestampInputParser.getMinutes());
+                String rewindedTime = rewindTimeByFactor(scanner.nextLine());
                 System.out.println(rewindedTime);
-            } catch (TimestampParserException e) {
-                System.out.println(e.getMessage());
+            } catch (DateTimeParseException e) {
+                System.out.println("Expected format is: H m");
             }
         }
     }
 
-    static String rewindTimeByFactor(int hour, int minute) {
-        LocalTime localTime = LocalTime.of(hour, minute, 0);
-        return localTime.minusMinutes(REWIND_BY_MINUTES_FACTOR).format(DateTimeFormatter.ofPattern("H m"));
-    }
-}
-
-class TimestampInputParser {
-
-    private int hours;
-    private int minutes;
-
-    void parse(String timestamp) {
-        validateInputIsNotEmpty(timestamp);
-        validateInputIsParsable(timestamp.split(" "));
-    }
-
-    private void validateInputIsNotEmpty(String timestamp) {
-        if (timestamp.isEmpty()) {
-            throw new TimestampParserException("Empty input");
-        }
-    }
-
-    private void validateInputIsParsable(String[] timeParts) {
-        try {
-            hours = Integer.parseInt(timeParts[0]);
-            minutes = Integer.parseInt(timeParts[1]);
-        } catch (NumberFormatException e) {
-            if (timeParts.length == 1 && Character.isDigit(timeParts[0].charAt(0))) {
-                throw new TimestampParserException("Unrecognized format");
-            }
-            throw new TimestampParserException("Cannot parse text");
-        } catch (IndexOutOfBoundsException e) {
-            throw new TimestampParserException("Unrecognized format");
-        }
-    }
-
-    public int getHours() {
-        return hours;
-    }
-
-    public int getMinutes() {
-        return minutes;
-    }
-}
-
-class TimestampParserException extends RuntimeException {
-    TimestampParserException(String message) {
-        super(message);
+    static String rewindTimeByFactor(String input) {
+        LocalTime localTime = LocalTime.parse(input, DateTimeFormatter.ofPattern(TIME_PATTERN));
+        return localTime.minusMinutes(REWIND_BY_MINUTES_FACTOR).format(DateTimeFormatter.ofPattern(TIME_PATTERN));
     }
 }
--- src/test/java/problems/SpavanacTest.java
+++ src/test/java/problems/SpavanacTest.java
@@ -3,66 +3,87 @@ package problems;
 import org.junit.Test;
 
 import java.time.DateTimeException;
+import java.time.format.DateTimeParseException;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class SpavanacTest {
 
     @Test
     public void test45minRewind() {
-        String resultingTime = Spavanac.rewindTimeByFactor(10, 10);
+        String resultingTime = Spavanac.rewindTimeByFactor("10 10");
         assertEquals("9 25", resultingTime);
     }
 
     @Test
     public void test24HourWrapping() {
-        String result = Spavanac.rewindTimeByFactor(0, 30);
+        String result = Spavanac.rewindTimeByFactor("0 30");
         assertEquals("23 45", result);
     }
 
     @Test
     public void testLateEvening() {
-        String result = Spavanac.rewindTimeByFactor(23, 40);
+        String result = Spavanac.rewindTimeByFactor("23 40");
         assertEquals("22 55", result);
     }
 
     @Test
     public void testEarlyMorning() {
-        String result = Spavanac.rewindTimeByFactor(1, 0);
+        String result = Spavanac.rewindTimeByFactor("1 0");
         assertEquals("0 15", result);
     }
 
     @Test
     public void testOneMinuteBeforeMidnight() {
-        String result = Spavanac.rewindTimeByFactor(23, 59);
+        String result = Spavanac.rewindTimeByFactor("23 59");
         assertEquals("23 14", result);
     }
 
     @Test
     public void testMidnight() {
-        String result = Spavanac.rewindTimeByFactor(0, 0);
+        String result = Spavanac.rewindTimeByFactor("0 0");
         assertEquals("23 15", result);
     }
 
     @Test
     public void testOneMinuteAfterMidnight() {
-        String result = Spavanac.rewindTimeByFactor(0, 1);
+        String result = Spavanac.rewindTimeByFactor("0 1");
         assertEquals("23 16", result);
     }
 
     @Test
-    public void testOnlySingleDigits() {
-        String result = Spavanac.rewindTimeByFactor(7, 46);
+    public void testOnlySingleDigitsResult() {
+        String result = Spavanac.rewindTimeByFactor("7 46");
         assertEquals("7 1", result);
+    }
+
+    @Test(expected = DateTimeParseException.class)
+    public void testUntrimmedInput() {
+        Spavanac.rewindTimeByFactor("       10 15      ");
+    }
+
+    @Test(expected = DateTimeParseException.class)
+    public void testTripleInput() {
+        Spavanac.rewindTimeByFactor("10 20 30");
+    }
+
+    @Test(expected = DateTimeParseException.class)
+    public void testEmptyInput() {
+        Spavanac.rewindTimeByFactor("");
+    }
+
+    @Test(expected = DateTimeParseException.class)
+    public void testWhitespaceInput() {
+        Spavanac.rewindTimeByFactor("         ");
     }
 
     @Test(expected = DateTimeException.class)
     public void testNotAnHour() {
-        Spavanac.rewindTimeByFactor(42, 30);
+        Spavanac.rewindTimeByFactor("42 30");
     }
 
     @Test(expected = DateTimeException.class)
     public void testNotAMinute() {
-        Spavanac.rewindTimeByFactor(0, 4711);
+        Spavanac.rewindTimeByFactor("0 4711");
     }
 }
